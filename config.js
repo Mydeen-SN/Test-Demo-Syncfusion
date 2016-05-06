b8ee95d32a2677caba9d612fbeb621df45e36eca
Mydeen-SN <mydeensn@syncfusion.com>
2016-05-06 12:34:55 +0530

    Update 2016-05-06T07:04:46.996Z

--- config.js
+++ config.js
@@ -425,7 +425,7 @@ System.config({
     }
   },
   "bundles": {
-    "app-build-e5e77cd689": [
+    "app-build-d07926576b": [
       "about/about",
       "about/about.html!github:systemjs/plugin-text@0.0.4",
       "app",
